8711ca2d852b024751bbeb28e561b4a073299743
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-12-26 13:27:33 +0100

    extend test script

--- scripts/fail.sh
+++ scripts/fail.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+random_sleep_time=$(awk -v min=1 -v max=5 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+sleep "$random_sleep_time"
 # just change to true and save while retry is running
 false
